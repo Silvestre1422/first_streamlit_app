ff7a7b186d84c421be07449b566bc0d93f61b395
Silvestre1422 <142042604+Silvestre1422@users.noreply.github.com>
2023-08-13 20:26:49 -0600

    Update streamlit_app.py
    urlError

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,6 +37,9 @@ my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 my_data_row = my_cur.fetchone()
+
+
+from urllib.error import URLError
 streamlit.text("Hello from Snowflake:")
 streamlit.text(my_data_row)
 
